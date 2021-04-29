531ac3c44a5bc919f852deac576daa2363d63483
Alexis Gp <a_aleguapal@iestriana.com>
2021-04-29 22:40:47 +0200

    task8

---
+++ Primero/js/task8.js
@@ -0,0 +1,19 @@
+function multiplicacion() {
+    var usuario = document.getElementById("usuario").value;
+    var numero = document.getElementById("numero").value;
+    var num1 = numero * 1
+    var num2 = numero * 2
+    var num3 = numero * 3
+    var num4 = numero * 4
+    var num5 = numero * 5
+    var num6 = numero * 6
+    var num7 = numero * 7
+    var num8 = numero * 8
+    var num9 = numero * 9
+    var num10 = numero * 10
+
+    document.getElementById("resultado").innerHTML = usuario + " esta es la tabla del " + numero + " ////////// " + numero + " * 1 = " + num1 + " ////////// " + numero + " * 2 = " + num2 + " ////////// " + numero + " * 3 = " + num3 + " ////////// " + 
+    numero + " * 4 = " + num4 + " ////////// " + numero + " * 5 = " + num5 + " ////////// " + numero + " * 6 = " + num6 + " ////////// " + numero + " * 7 = " + num7 + " ////////// " + numero + " * 8 = " + num8
+    + " ////////// " + numero + " * 9 = " + num9 + " ////////// " + numero + " * 10 = " + num10 
+    
+}
